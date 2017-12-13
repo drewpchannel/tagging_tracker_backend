2e57222dc6db2cb91166f6744c63f3b4cf25ba45
Aaron McRuer <a.mcruer@live.com>
2017-12-13 11:42:52 -0600

    editing gunicorn settings again with 0.0.0.0 and fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,7 +11,7 @@ echo 'Starting Gunicorn.'
 exec gunicorn tagging_tracker.wsgi:application \
     --name tagging_tracker \
     #--bind unix:tagging_tracker.sock \
-    --bind 0.0.0.0:80
+    --bind 0.0.0.0:80 \
     --workers 3 \
     --log-level=info \
     --log-file=/code/logs/gunicorn.log \
